87a2319d4a2bc1ca02172123a43ace705d6af71f
Adamiscool123 <adamidrissi177@gmail.com>
2026-05-25 08:03:37 -0400

    Refactor data loading and processing for moving average calculation

--- 2_Strategy/Moving_Average/main.py
+++ 2_Strategy/Moving_Average/main.py
@@ -2,5 +2,14 @@ import pandas as pd
 
 data_path = "/home/adamidrissi/Quant-Engine/1_Data/^GSPC.csv"
 
-with open(data_path, "r") as f:
-    data = f.read()
+df = pd.read_csv(data_path)
+
+df_cleaned = df.dropna()
+
+moving_average = df_cleaned['SMA_200']
+
+close_price = df_cleaned['Close']
+
+date = df_cleaned['Date']
+
+print(moving_average)
