1b35d40253b1b47eff2a318f9e516158e7a46dc5
Adamiscool123 <adamidrissi177@gmail.com>
2026-05-26 19:54:41 +0100

    Set backtester base

--- 3_Backtester/main.py
+++ 3_Backtester/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
-dataset = "C:\Users\adami\OneDrive\Documents\quant_engine\2_Strategy\Moving_Average\buy_sell_signals.csv"
+dataset_signals = "C:\Users\adami\OneDrive\Documents\quant_engine\2_Strategy\Moving_Average\buy_sell_signals.csv"
 
-df = pd.read_csv(dataset)
+dataset_stock = "C:\Users\adami\OneDrive\Documents\quant_engine\1_Data\GSPC.csv"
+
+df_signals = pd.read_csv(dataset_signals)
+
+df_stock = pd.read_csv(dataset_stock)
 
