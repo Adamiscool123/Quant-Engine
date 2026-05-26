955cd3195fa2cfd991f0312d1bb5d4526238df8b
Adamiscool123 <adamidrissi177@gmail.com>
2026-05-26 07:55:24 +0100

    Fixed problem

--- 2_Strategy/Moving_Average/main.py
+++ 2_Strategy/Moving_Average/main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import csv
 
-data_path = "/home/adamidrissi/Quant-Engine/1_Data/^GSPC.csv"
+data_path = "C:/Users/adami/OneDrive/Documents/quant_engine/1_Data/GSPC.csv"
 
 # 1. Skip the broken header rows (rows 0, 1, 2) and name them explicitly
 column_names = ['Date', 'Close', 'High', 'Low', 'Open', 'Volume', 'SMA_200']
 df = pd.read_csv(data_path, skiprows=3, names=column_names)
 
 # 2. Clean up any empty rows
-df_cleaned = df.dropna(subset=['Close', 'SMA_200'])
+df_cleaned = df.dropna(subset=['Close', 'SMA_200']).copy()
 
 # 3. Extract your columns safely
 moving_average = df_cleaned['SMA_200']
@@ -23,8 +23,8 @@ sell = 0
 with open("buy_sell_signals.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerow(['Date', 'Close', 'SMA_200', 'Signal']) 
-    
-    for i in range(-1, len(close_price)):
+
+    for i in range(0, len(close_price)):
         if close_price.iloc[i] > moving_average.iloc[i]:
             f.write(f"{date.iloc[i]},{close_price.iloc[i]},{moving_average.iloc[i]},Buy\n")
         else:
---
+++ 3_Backtester/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+dataset = "C:\Users\adami\OneDrive\Documents\quant_engine\2_Strategy\Moving_Average\buy_sell_signals.csv"
+
+df = pd.read_csv(dataset)
+
